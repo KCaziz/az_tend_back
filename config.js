1031fa9a6c8dd596138858fe979d4ebc3129fc38
KCaziz <114615343+KCaziz@users.noreply.github.com>
2024-09-22 22:45:50 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+export const PORT = 5555;
+export const mongoDBURL =
+  "mongodb+srv://aztenders:aztenders24%40@az-tenders.l7avv.mongodb.net/?retryWrites=true&w=majority&appName=az-tenders";
+export const JWT_SECRET = "Matoub_3emek";
+export const JWT_EXPIRES_IN = "90d";
+
+export const CLOUDINARY_CLOUD_NAME = "dzugplucv";
+export const CLOUDINARY_API_KEY = "574761836194316";
+export const CLOUDINARY_API_SECRET = "a40s_tuYU1jaJJbUcq2D344ezfs";
