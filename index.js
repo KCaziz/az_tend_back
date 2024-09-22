1cec68b4f7664df72579056899a5496c6cac6f62
KCaziz <114615343+KCaziz@users.noreply.github.com>
2024-09-22 18:20:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(passport.session());
 // cors middleware
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://az-tend-back.onrender.com/",
     methods: ["GET", "PUT", "DELETE", "POST", "PATCH"],
   })
 );
